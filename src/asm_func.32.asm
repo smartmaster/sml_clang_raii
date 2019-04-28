5e3eec6a538bee23d9a2522359977ea1a5797b50
smart.master@live.com <smart.master@live.com>
2019-04-28 22:34:30 +0800

    change __stdcall to __cdecl call, it should be a vs2019 bug that vs2019 cannot recognize __stdcall
    Signed-off-by: smart.master@live.com <smart.master@live.com>

--- src/asm_func.32.asm
+++ src/asm_func.32.asm
@@ -11,7 +11,7 @@ STACK_SIZE equ 16*4
 ;BYTE* Sml_GetRetAddr(SmlCVector* vec_blocks, long initing_local, BYTE** pretaddr);
 extern _Sml_GetRetAddr :  proc
 
-;SML_DATA_PTR Sml_AsmGetRetAddr(PSmlCVector vec, int enabled, int reserved1, int reserved2);
+;SML_DATA_PTR /*__stdcall*/ Sml_AsmGetRetAddr(PSmlCVector vec, int enabled, int reserved1, int reserved2);
 _Sml_AsmGetRetAddr proc public
 
 
@@ -37,8 +37,10 @@ _Sml_AsmGetRetAddr endp
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;//void Sml_AsmPrepareAndRunCleanups(LONG localIniting, LONG volatile* globalInited, SmlCVector* vec_jmp);
+;void Sml_LinkAndRunCleanups(LONG localIniting, LONG volatile* globalInited, SmlCVector* vec_jmp, LPBYTE* pretddr);
 extern _Sml_LinkAndRunCleanups : proc
+
+;void /*__stdcall*/ Sml_AsmLinkAndRunCleanups(LONG localIniting, LONG volatile* globalInited, SmlCVector* vec_jmp);
 _Sml_AsmLinkAndRunCleanups proc public
 
 sub esp, STACK_SIZE
--- src/asm_func.h
+++ src/asm_func.h
@@ -14,10 +14,10 @@ __declspec(noinline)
 void Sml_AsmLinkAndRunCleanups(LONG localIniting, LONG volatile* globalInited, SmlCVector* vec_jmp);
 #else
 __declspec(noinline)
-SML_DATA_PTR __stdcall Sml_AsmGetRetAddr(PSmlCVector vec, int enabled, int reserved1, int reserved2);
+SML_DATA_PTR /*__stdcall*/ Sml_AsmGetRetAddr(PSmlCVector vec, int enabled, int reserved1, int reserved2);
 
 __declspec(noinline)
-void __stdcall Sml_AsmLinkAndRunCleanups(LONG localIniting, LONG volatile* globalInited, SmlCVector* vec_jmp);
+void /*__stdcall*/ Sml_AsmLinkAndRunCleanups(LONG localIniting, LONG volatile* globalInited, SmlCVector* vec_jmp);
 #endif
 
 EXTERN_C_END
